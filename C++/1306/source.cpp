ce1d71789475bb7def9f2b8dc302fac1fa886dde
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-06-02 17:29:43 +0900

    2024-06-02-baekjoon_1306

---
+++ C++/1306/source.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#ifndef __SEGMENT_TREE_HPP__
+#define __SEGMENT_TREE_HPP__
+
+#include <vector>
+
+template <typename T>
+class SegmentTree {
+private:
+    std::vector<T> tree;
+    std::vector<T> arr;
+    size_t n;
+
+    void build(size_t idx, size_t left, size_t right) {
+        if (left == right) {
+            tree[idx] = arr[left];
+            return;
+        }
+
+        size_t mid = (left + right) / 2;
+
+        build(2 * idx, left, mid);
+        build(2 * idx + 1, mid + 1, right);
+
+        const T& leftValue = tree[2 * idx];
+        const T& rightValue = tree[2 * idx + 1];
+
+        tree[idx] = std::max(leftValue, rightValue);
+    }
+
+    const T query(size_t i, size_t j, size_t node, size_t currentLeft, size_t currentRight) {
+        if (j < currentLeft || currentRight < i) {
+            return 0;
+        }
+        else if (i <= currentLeft && currentRight <= j) {
+            return tree[node];
+        }
+        else {
+            size_t mid = (currentLeft + currentRight) / 2;
+            const T leftValue = query(i, j, 2 * node, currentLeft, mid);
+            const T rightValue = query(i, j, 2 * node + 1, mid + 1, currentRight);
+
+            return std::max(leftValue, rightValue);
+        }
+    }
+    
+
+public:
+    SegmentTree(const std::vector<T>& a) : arr(a), n(a.size()) {
+        tree.resize(4 * n + 1);
+        build(1, 0, n - 1);
+    }
+
+    const T query(size_t i, size_t j) {
+        return query(i, j, 1, 0, n - 1);
+    }
+};
+
+#endif // !__SEGMENT_TREE_HPP__
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N, M;
+    cin >> N >> M;
+    
+    vector<int> intensity(N);
+    
+    for (int i=0; i<N; i++) {
+        cin >> intensity[i];
+    }
+    
+    SegmentTree<int> segtree(intensity);
+    
+    for (int i=M-1; i<N-M+1; i++) {
+        cout << segtree.query(i - (M-1), i + (M-1)) << " ";
+    }
+    
+    return 0;
+}
