f2be0da30e88539474e47e4b0e243e7022b5fca9
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-06-12 19:51:32 +0900

    2024-06-12 baekjoon_31611

---
+++ C++/31611/source.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int X;
+    cin >> X;
+    
+    cout << (X % 7 == 2);
+    
+    return 0;
+}
