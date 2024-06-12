e871386381e73b6d97d1c70ffaa658a922134745
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-06-12 23:50:41 +0900

    2024-06-12-baekjoon_31606

---
+++ C++/31606/source.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int A, B;
+    cin >> A >> B;
+    
+    cout << A + B + 3;
+    
+    return 0;
+}
