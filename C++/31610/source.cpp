f17a2ccc66e124d4878220df40ee86691066ca59
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-06-25 08:11:39 +0000

    2024-06-25-baekjoon_31610

---
+++ C++/31610/source.cpp
@@ -0,0 +1,17 @@
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    cout << A * B + C;
+    
+    return 0;
+}
