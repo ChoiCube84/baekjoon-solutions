ee6e3d00a751e6ac968b1f5f84e22f6b6cc68cee
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-07-06 15:00:22 +0000

    2024-07-06-baekjoon_2306

--- C++/2306/source.cpp
+++ C++/2306/source.cpp
@@ -8,6 +8,8 @@ using ull = unsigned long long int;
 
 using pll = pair<ll, ll>;
 
+bool matchedDNA(char a, char b);
+
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -22,9 +24,21 @@ int main(void) {
 
     for (int end=1; end<n; end++) {
         for (int start=end-1; start>=0; start--) {
-            // Lorem ipsum
+            if (matchedDNA(dna[start], dna[end])) {
+                dp[start][end] = dp[start+1][end-1] + 2;
+            }
+
+            for (int middle=start; middle<end; middle++) {
+                dp[start][end] = max(dp[start][end], dp[start][middle] + dp[middle+1][end]);
+            }
         }
     }
 
+    cout << dp[0][n-1];
+
     return 0;
+}
+
+bool matchedDNA(char a, char b) {
+    return (a == 'a' && b == 't') || (a == 'g' && b == 'c');
 }
