f1d41a4a24862630a690ac3e2c07aadbe0bac843
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-08-26 13:46:19 +0000

    2024-08-26-baekjoon_16878_20096

--- C++/16878/source.cpp
+++ C++/16878/source.cpp
@@ -20,36 +20,31 @@ int main(void) {
 
 	ll MOD = 1'000'000'007;
 	ll largestFound = 3;
-	
+
 	dp[0] = 1;
 	dp[1] = 1;
 	dp[2] = 0;
 	dp[3] = 0;
-	
+
 	while (T--) {
 		ll N;
 		cin >> N;
 
 		if (N > largestFound) {
 			for (ll i=largestFound+1; i<=N; i++) {
-				// https://oeis.org/A002464
 				dp[i] = (i+1) * dp[i-1] - (i-2) * dp[i-2] - (i-5) * dp[i-3] + (i-3) * dp[i-4];
+				dp[i] %= MOD;
 
-				if (dp[i] >= MOD) {
-					dp[i] %= MOD;
-				}
-				else if (dp[i] < 0) {
-					while (dp[i] < 0) {
-						dp[i] += MOD;
-					}
+				if (dp[i] < 0) {
+					dp[i] += MOD;
 				}
 			}
 
 			largestFound = N;
 		}
-		
+
 		cout << dp[N] << '\n';
 	}
-	
+
 	return 0;
 }
