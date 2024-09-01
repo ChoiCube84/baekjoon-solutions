f19080a87a85ad61cae5c2c121cc65bb55d3af85
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-09-01 20:52:43 +0900

    2024-09-01-baekjoon_15372

---
+++ C++/15372/source.cpp
@@ -0,0 +1,27 @@
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+using ll = long long int;
+using ull = unsigned long long int;
+
+using pll = pair<ll, ll>;
+using ld = long double;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    ll T;
+    cin >> T;
+    
+    while (T--) {
+        ll N;
+        cin >> N;
+        
+        cout << N * N << '\n';
+    }
+    
+    return 0;
+}
