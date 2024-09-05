22be93d589b3cde940229a1cd4b36ee99882f9c6
ChoiCube84 <79388736+ChoiCube84@users.noreply.github.com>
2024-09-05 22:12:19 +0900

    2024-09-05-baekjoon_10813

---
+++ C++/10813/source.cpp
@@ -0,0 +1,37 @@
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+using ll = long long int;
+using ull = unsigned long long int;
+
+using ld = long double;
+using pll = pair<ll, ll>;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	ll N, M;
+	cin >> N >> M;
+
+	vector<ll> baskets(N+1, 0);
+
+	for (ll i=1; i<=N; i++) {
+		baskets[i] = i;
+	}
+
+	while (M--) {
+		ll i, j;
+		cin >> i >> j;
+
+		swap(baskets[i], baskets[j]);
+	}
+
+	for (ll i=1; i<=N; i++) {
+		cout << baskets[i] << ' ';
+	}
+
+	return 0;
+}
