2da1fe2f5cb927c7c01d9703cfaf0de92a3a0b06
ChoiCube84 <wldnd84@naver.com>
2024-11-24 12:54:42 +0000

    2024-11-24-baekjoon_3765

---
+++ 3765/source.cpp
@@ -0,0 +1,22 @@
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+using ld = long double;
+using pll = pair<ll, ll>;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	string input;
+	while (cin >> input) {
+		cout << input << '\n';
+	}
+	
+	return 0;
+}
