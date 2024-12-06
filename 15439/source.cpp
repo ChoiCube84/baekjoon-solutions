da43127ca8bf89472df3961199f9fa8d6e611048
ChoiCube84 <wldnd84@naver.com>
2024-12-06 13:19:18 +0000

    2024-12-06-baekjoon_15439

---
+++ 15439/source.cpp
@@ -0,0 +1,22 @@
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+using ld = long double;
+using pll = pair<ll, ll>;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	ll N;
+	cin >> N;
+	
+	cout << N * (N-1);
+	
+	return 0;
+}
