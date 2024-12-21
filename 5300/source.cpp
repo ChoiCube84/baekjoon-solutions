1b84061cc5bc76d2ee5e3aad342167d4eba9a13c
ChoiCube84 <wldnd84@naver.com>
2024-12-21 13:05:02 +0000

    2024-12-21-baekjoon_5300

---
+++ 5300/source.cpp
@@ -0,0 +1,28 @@
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+
+using ld = long double;
+using pll = pair<ll, ll>;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	ll N;
+	cin >> N;
+	
+	for (ll i=1; i<=N; i++) {
+		cout << i << ' ';
+		
+		if (i % 6 == 0 || i == N) {
+			cout << "Go! ";
+		}
+	}
+	
+	return 0;
+}
